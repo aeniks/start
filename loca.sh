a2696411e98c2508b2f70e57d68908297a4281ce
aa <leonel@aeniks.com>
2025-02-09 11:30:14 +0100

    2025-02-09_11_30

--- loca.sh
+++
@@ -1,4 +0,0 @@
-latlong=($(cat  $HOME/logs/loc.log|grep "latitude" -A1 -m1|cut -f4 -d " "|tr "," " ")); 
-(echo; date; wget -qO- \
-"https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlong[0]},${latlong[1]}&location_type=ROOFTOP&result_type=street_address&key=AIzaSyDaCqrwXUY9E2u56IN-RWXc7X0G1CVICYg"|\
-grep "formatted_address"|cut -f2 -d":") >> $HOME/logs/logcation.log
