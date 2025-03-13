5d23c70776290505e72e3984d04d390208d78178
aeniks <aeniks>
2025-03-13 13:04:05 +0100

    k

--- crons/locup.sh
+++ crons/locup.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-(cp /data/data/com.termux/files/home/logs/nloc.log \
-/data/data/com.termux/files/home/gh/aeniks.github.io/loc.html --update; 
+(cat /data/data/com.termux/files/home/logs/nloc.log|aha -l > \
+/data/data/com.termux/files/home/gh/aeniks.github.io/loc.html; 
 cd /data/data/com.termux/files/home/gh/aeniks.github.io/; 
 git commit -a -m "$(date)"; git push; date; )
