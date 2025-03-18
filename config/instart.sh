e0fa159727d66a12bcec3c57b6bdc1e022df2e41
aeniks <aeniks>
2025-03-18 11:21:00 +0100

    2025-03-18_11_21

--- config/instart.sh
+++ config/instart.sh
@@ -3,9 +3,14 @@
 mkdir $HOME/tmp 2>/dev/null; 
 mv $HOME/.inputrc $HOME/tmp/ 2>/dev/null; 
 ln $HOME/start/config/inputrc $HOME/.inputrc --symbolic; 
+echo 'ln $HOME/start/config/inputrc $HOME/.inputrc --symbolic; '; 
 mv $HOME/.tmux.conf $HOME/tmp/ 2>/dev/null; 
 ln $HOME/start/config/tmux/tmux.conf $HOME/.tmux.conf --symbolic; 
+echo 'ln $HOME/start/config/tmux/tmux.conf $HOME/.tmux.conf --symbolic; '; 
 mv $HOME/.tmux.conf.local $HOME/tmp/ 2>/dev/null; 
 ln $HOME/start/config/tmux/tmux.conf.local $HOME/.tmux.conf.local --symbolic; 
+echo 'ln $HOME/start/config/tmux/tmux.conf.local $HOME/.tmux.conf.local --symbolic; '; 
 rm $HOME/.config/lf -r 2>/dev/null; 
 ln $HOME/start/config/lf $HOME/.config/ --symbolic; 
+echo 'ln $HOME/start/config/lf $HOME/.config/ --symbolic; '; 
+echo -e "\n\n -- done!\n\n"
