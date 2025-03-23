357892699bcc7c5ff7a50ac276c9f14686444e5d
localhost <localhost>
2025-03-23 04:34:43 +0100

    2025-03-23_04_34

--- funcs/lf.sh
+++ funcs/lf.sh
@@ -1,5 +1,5 @@
 alias l='lf -print-selection -last-dir-path $HOME/.lfp.log; cd $(cat $HOME/.lfp.log); \
 lfc=($(tail -n+2 $HOME/.local/share/lf/files|tee $HOME/.lfc.log 2>/dev/null; )); \
 test -n $lfc && printf %b "\e[0mcopy-selected: \e[36m\n${lfc[*]}\e[0m\n"; 
-lft=($(tail -n+2 $HOME/.local/share/lf/tags|tee $HOME/.lfc.log 2>/dev/null; )); \
-test -n $lft && printf %b "\e[0mtags-selected: \e[35m\n${lfc[*]}\e[0m\n"; '; 
+lft=($(tail -n+2 $HOME/.local/share/lf/tags|tee $HOME/.lft.log 2>/dev/null; )); \
+test -n $lft && printf %b "\e[0mtags-selected: \e[35m\n${lft[*]}\e[0m\n"; '; 
