6898262983bc3ee9d3e9bb9cffb6d15a29f51835
bobo krull <163834223+aeniks@users.noreply.github.com>
2025-03-29 23:27:39 +0100

    Update instart.sh

--- config/instart.sh
+++ config/instart.sh
@@ -9,18 +9,22 @@ cyan='\e[36m' yellow='\e[33m' blue='\e[36m' bold='\e[1m' \
 height="$(stty size|cut -f1 -d" ")" width="$(stty size|cut -f2 -d" ")" \
 yno='\e[0m[\e[2mY\e[0m/\e[2mn\e[0m]' c2='\e[0m\e[36m--\e[0m' uu="60" \
 enter='\e[0m[\e[2mq\e[0m]\e[2muit \e[0mor [\e[2mENTER\e[0m]' x="2>/dev/null"; 
+conf=(rclone lf micro tmux htop gh); 
+####
+apts_basic=(file libexif-dev openssl openssh-server \
+rsync rclone w3m w3m-img googler exiftool \
+mediainfo figlet lolcat lynx fortune-mod links2 \
+toilet iproute2 net-tools nmap fastfetch \
+neofetch fzf fortune fortune-mod fortunes \
+ccze lf bat batcat btop htop ncdu \
+bash-completion lsd tmux git gh nodejs \
+nmap texinfo aha micro fortunes gnupg \
+wget wget2 curl aria2 gh git rclone rsync iw timg); 
+####
+apts_extra=(ffmpeg mpv);
 ####
 (hash sudo 2>/dev/null && sudo="sudo"; hash sudo 2>/dev/null||alias sudo=' ' 2>/dev/null; 
 sudo apt update &>/dev/null && sudo apt install -y git &>/dev/null )& disown &>/dev/null;
-####
-
-
-
-# )& disown &>/dev/null; 
-#(apt update &>/dev/null && apt install -y git &>/dev/null) & disown &>/dev/null; ps -e
-####
-
-
 ####
 unalias p1 p2 2>/dev/null; 
 p1() { p2=" ${@}"; for i in $(seq ${#p2}); do sleep .1; printf %b "${p2:${i}:1}"; done; }; ## rolling text 
@@ -36,12 +40,12 @@ echo;echo;
 for i in $(seq $((height - 2))); do printf %b "\e[38;5;$((RANDOM%16 + 111))m$i\n"; sleep .04; done; ## scroll page 
 for i in $(seq $((height - 2))); do printf %b "\e[K\e[A\e[2K"; sleep .04; done; 
 ####
-p2 " $c2 "; p1 "Download config files? "; p2 "\e[1m $enter"; 
+p2 " $c2 "; p1 "Download config files? "; p2 "\e[1m$enter "; 
 read -rsn1 "ny"; [ $ny ]&& printf %b "$green OK$re\n\n" && return 0; 
-printf %b "\e[48G      \e[8D  "; p2 "\e[0;1m [\e[0;92m"; p1 "OK"; p2 "\e[0;1m]  \e[0m\n"; 
+printf %b "\e[60G      \e[8D  "; p2 "\e[0;1m [\e[0;92m"; p1 "OK"; p2 "\e[0;1m]  \e[0m\n"; 
 p2 " $c2 "; p1 "Where to? "; read -ei "$HOME/" "start1"; start="${start1}/start"; 
 printf %b "\e[A\e[48G      \e[8D  "; p2 "\e[0;1m [\e[0;92m"; p1 "OK"; p2 "\e[0;1m]  \e[0m\n";
-########
+####
 _backup "$start"; _newcolor; 
 git clone https://github.com/aeniks/start.git $start&& \
 mv $start/.git/config $start/.git/config_old; 
@@ -49,18 +53,14 @@ printf %b '[core]\n  repositoryformatversion = 0 \n  filemode = true\n  bare = f
 logallrefupdates = true\n  [remote "origin"]\n  url = git@github.com:aeniks/start.git
 fetch = +refs/heads/*:refs/remotes/origin/*\n  [branch "main"]\n  remote = origin
 merge = refs/heads/main\n  [pull]\n  rebase = true' > $start/.git/config; cd $start; 
-########
-########
-p2 " $c2 "; p1 "Install config?"; p2 "\e[1m $enter"; 
+####
+p2 " $c2 "; p1 "Install config? "; p2 "\e[1m$enter "; 
 read -rsn1 "ny"; [ $ny ]&& printf %b "$green OK$re\n\n" && return 0; 
-p2 " $c2 \e[A\e[$((width / 2 - 12))G\e[0m[\e[92m" p1 OK; p2 "\e[0m]\n";
-# printf %b "$green OK$re"; sleep .2; echo; sleep .2; echo; 
+printf %b "\e[60G      \e[8D  "; p2 "\e[0;1m [\e[0;92m"; p1 "OK"; p2 "\e[0;1m]  \e[0m\n"; 
 $sudo mv $PREFIX/etc/lf $tmp/ 2>/dev/null; _newcolor; 
 $sudo ln $start/config/lf $PREFIX/etc/ -s  2>/dev/null; _newcolor; 
 mkdir $HOME/.config 2>/dev/null; cd $HOME/.config;  _newcolor; 
 echo; 
-####
-conf=(rclone lf micro tmux htop gh); 
 ####
 for q in ${conf[*]}; do 
 _move $HOME/.config/$i $tmp/; 
@@ -68,34 +68,18 @@ _link $start/config/$q $HOME/.config/ -s; sleep .2;
 _newcolor; printf %b " $q"; 
 printf %b "\n\e[0m"; p1 "updated"; 
 done; echo; cd; 
-# _move "rclone lf micro tmux htop gh" $tmp/; _newcolor; 
-# cd $start/config/;  	_newcolor; 
-# _link "rclone lf micro tmux htop gh" $HOME/.config/; _newcolor; 
-##### 
+#### 
 _newcolor; printf %b "\e[0m\t\t"; 
 cat $HOME/.bashrc|grep -e "anew.sh" &>/dev/null||\
 printf %b "\n. $start/anew.sh;"&>/dev/null >> $HOME/.bashrc 2>/dev/null; 
-#printf %b "\n. $cyan$/start/anew.sh$re >> $green$HOME/.bashrc\n"; 
 touch $HOME/._tmux 2>/dev/null; chmod 775 $HOME/._tmux; echo; 
-#######
-########
-########
+####
 _move "$HOME/.inputrc" $tmp/;  	_newcolor; 
 _move "$HOME/.tmux.conf $HOME/.tmux.conf.local" $tmp/; 	_newcolor; 
 _link $start/config/inputrc $HOME/.inputrc;  	_newcolor; 
-########
-#mv $HOME/.tmux.conf $HOME/tmp/ 2>/dev/null; 
-#ln $HOME/start/config/tmux/tmux.conf $HOME/.tmux.conf --symbolic; 
-########
-#sleep .1; printf %b "\e[38;5;4$((u++))m"; ## newcolor 
-#mv $HOME/.tmux.conf.local $HOME/tmp/ 2>/dev/null; 
-#ln $HOME/start/config/tmux/tmux.conf.local $HOME/.tmux.conf.local --symbolic; 
-########
-#sleep .1; printf %b "\e[38;5;4$((u++))m"; ## newcolor 
 _move "$HOME/.tmux_bash.sh" $tmp/;  	_newcolor; 
 _link "$start/config/tmux/tmux_bash.sh" $HOME/.tmux_bash.sh;  	_newcolor; 
-########
-########
+####
 sleep .2; printf %b "\n $c2\e[2m [\e[0mDONE\e[2m]\e[0m!\n"; sleep .2;  	_newcolor; 
 mkdir -p -m 775 $PREFIX/usr/share/figlet 2>/dev/null||\
 $sudo mkdir -p -m 775 $PREFIX/usr/share/figlet 2>/dev/null; 
@@ -107,34 +91,16 @@ hash sudo 2>/dev/null && sudo="sudo";
 sudo cp $start/start/config/ssss.sh $PREFIX/bin/ssss; 
 printf %b "\n\n\n\n\e[4A\n $c2"; p1 " Install apps"; p2 "\e[1m? $yno "; 
 read -esn1 "ny"; [ $ny ]&& printf %b "\t\t $green OK$re\n\n" && return 0; 
-p2 " $c2 "; p1 "updating system ..."; echo;echo; 
-tput setaf 5 2>/dev/null; $sudo apt update; 
-tput setaf 5 2>/dev/null; $sudo pkg update -y; 
-tput setaf 6 2>/dev/null; $sudo apt upgrade -y; 
-tput setaf 4 2>/dev/null; echo ok; 
+p2 " $c2 "; p1 "updating system ..."; echo; echo; _newcolor; 
+$sudo apt update;  _newcolor; $sudo apt upgrade -y; _newcolor; echo; 
+printf %b "\e[60G      \e[8D  "; p2 "\e[0;1m [\e[0;92m"; p1 "OK"; p2 "\e[0;1m]  \e[0m\n"; 
 ####
-apts_basic=(file libexif-dev openssl openssh-server \
-rsync rclone w3m w3m-img googler exiftool \
-mediainfo figlet lolcat lynx fortune-mod links2 \
-toilet iproute2 net-tools nmap fastfetch \
-neofetch fzf fortune fortune-mod fortunes \
-ccze lf bat batcat btop htop ncdu \
-bash-completion lsd tmux git gh nodejs \
-nmap texinfo aha micro fortunes gnupg \
-wget wget2 curl aria2 gh git rclone rsync iw timg); 
-########
-apts_extra=(ffmpeg mpv);
-########
-
-########
 [ -e "$HOME/logs/apa.log" ]||apt list>$tmp/x; 
 tail -n+1 $tmp/x|cut -f1 -d"/">$HOME/logs/apa.log; 
-########
+####
 apts_install=($(for i in ${apts_basic[*]}; do hash $i 2>/dev/null || \
 grep $HOME/logs/apa.log -x -e "$i"; done; )); 
-########
-########
-
+####
 for i in ${apts_install[*]}; do 
 hash $i 2>/dev/null && printf %b "\n$reSkipping $cyan $i$re already installed$green"; 
 printf %b "\e[38;5;$((uu++))m\n"; 
