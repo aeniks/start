eea545729f26a591911dd8977e0e3b646b1481c8
u0_a428 <u0_a428@localhost>
2025-04-01 10:47:49 +0200

    2025-04-01_10_47

--- crons/bat.sh
+++ crons/bat.sh
@@ -1,5 +1,5 @@
 termux-battery-status > ~/logs/battery.log; 
 bat="$(cat ~/logs/battery.log |grep -e "percentage"|tr -d 'A-z ,\":';)"; 
 b="$(seq -w 000 $((bat - 1))|tail -n1|cut -c2)"; 
-bc="\e[2;36m"; cat ~/logs/battery.log|grep -e "CHARGING" &>/dev/null && bc="\e[1;92m";
+bc="\e[1;92m"; cat ~/logs/battery.log|grep -e "DISCHARGING" &>/dev/null && bc="\e[0;97m";
 printf %b "${bc}[\e[0m\e[9${b}m${bat}${bc}]\e[0m" > ~/logs/bat.sh; 
