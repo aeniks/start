ba401efac1ef55b9e0b32ee26b6c4542270cc5fd
u0_a64 <u0_a64@localhost>
2025-04-04 19:00:08 +0200

    2025-04-04_19_00

--- _ps1.sh
+++ _ps1.sh
@@ -9,5 +9,5 @@ cat $HOME/logs/bp.log 2>/dev/null && printf %b "\e[0m ";
 _ps1() { 
 PS1=''$re'[\e[0;1;38;5;$((2 + $?))m$?'$re']'$re$ipp$re'\
 ['$re'\e[1m\e[38;5;$((RANDOM%88 + 88))m${mod:0:8}'$re'] \
-['$re$cyan'\u'$re'] '$re'$(batt)''\
+['$re$cyan'\u'$re']'$re'$(uptime|cut -f11 -d" "|tr -d ",")''\
 ['$re$yellow'\w'$re']\e[25h\e[0m\n'; }; 
