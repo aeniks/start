b82a153cb09080c74c492302e563b645b8237a4e
u0_a64 <u0_a64@localhost>
2025-04-05 03:00:14 +0200

    2025-04-05_03_00

--- _ps1.sh
+++ _ps1.sh
@@ -3,11 +3,11 @@
 batt() { 
 [ -e $HOME/logs/bp.log ] 2>/dev/null &&
 printf %b "\e[9$(cat $HOME/logs/bp.log|cut -c1)m" &&
-cat $HOME/logs/bp.log 2>/dev/null && printf %b "\e[0m ";
+cat $HOME/logs/bp.log 2>/dev/null && printf %b "\e[0m "; cat "/sys/class/power_supply/BAT1/capacity" 2>/dev/null;
 }; 
 
 _ps1() { 
 PS1=''$re'[\e[0;1;38;5;$((2 + $?))m$?'$re']'$re$ipp$re'\
 ['$re'\e[1m\e[38;5;$((RANDOM%88 + 88))m${mod:0:8}'$re'] \
-['$re$cyan'\u'$re']'$re'$(uptime|cut -f11 -d" "|tr -d ",")''\
+['$re$cyan'\u'$re']'$re$dim' $(batt||uptime|cut -f11 -d" "|tr -d ",") '$re'\
 ['$re$yellow'\w'$re']\e[25h\e[0m\n'; }; 
