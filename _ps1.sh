4b9961633db9648056caefe0d7b0597a757076cf
u0_a428 <u0_a428@localhost>
2025-04-08 04:42:44 +0200

    2025-04-08_04_42

--- _ps1.sh
+++ _ps1.sh
@@ -13,11 +13,11 @@ _bat() {
 if [ -e "/sys/class/power_supply/BAT0/status" ]; \
 then printf -v "batstat" "/sys/class/power_supply/BAT0/status"; \
 printf -v "batcap" "/sys/class/power_supply/BAT0/capacity"; 
-else printf -v "batstat" "$HOME/logs/bp.log"; 
+else printf -v "batstat" "$HOME/logs/battery.log"; 
 printf -v "batcap" "$HOME/logs/bp.log"; fi; 
 printf -v "bs" "\e[0;2m"; 
 cat $batstat 2>/dev/null|grep -i "Charging" --quiet && \
-printf -v bs '\e[0;92m'; 
+printf -v "bs" "\e[0;92m"; 
 printf -v "bc" "$(cat $batcap)"; 
 printf %b "$bc" > $HOME/logs/bpc.log; 
 printf -v "bat" "\e[${bc}m$bc"; 
