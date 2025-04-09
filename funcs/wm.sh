971aeb01ffe15ef3ed6adc354e6c70f78707dbf3
u0_a428 <u0_a428@localhost>
2025-04-09 21:06:59 +0200

    2025-04-09_21_06

--- funcs/wm.sh
+++ funcs/wm.sh
@@ -1,3 +1,9 @@
 
-wm() { [ $1 ] && w="$@"; while true; do printf %b "\e[H\e[J\e[?25l\n\n\n\n\n\n"; f="$(shuf -n1 $HOME/figz.sh)"; figlet -f $f -w $COLUMNS -c "$(printf %b "$w";)"|bat -ppflzig --theme Dracula ; printf %b "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n$f"; 
+wm() { [ $1 ] && w="$@"; 
+ff=($(cat figz.sh|shuf)); 
+while true; do printf %b "\e[H\e[J\e[?25l\n"; 
+f="$(shuf -n1 $HOME/figz.sh)"; figlet -f $f -w $COLUMNS -c "$(printf %b "$w";)"|bat -ppflzig --theme Dracula ; 
+
+printf %b "\e7\n\e[$((LINES - 4))H$f\e8"; 
+
 read -n1 -s ll; [[ $ll ]] && printf %b "\n\nok\n\e[0m\e[?25h" && break; done; }; 
