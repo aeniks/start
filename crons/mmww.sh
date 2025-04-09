1bcdfcf65686b1a038447c1ed58cce2b65684705
u0_a64 <u0_a64@localhost>
2025-04-09 12:56:01 +0200

    2025-04-09_12_56

--- crons/mmww.sh
+++ crons/mmww.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash 
 time="$(date +%Y_%B_%d)"; 
-w3m -dump  https://www.merriam-webster.com/word-of-the-day|head -n66|grep ":" -m1 -A22|tr -s "\n\t" "\n." |sed -e /play/d -e /Prev\ Next/d -e /What\ It\ Means/d -e "/Context.*/d"|sed -e "s/\: / printf -v da\ '\n/g" -e 1a"';\n\n printf -v w '" -e "2a';\n\n printf -v meaning '" -e 4c"'; \n\n printf -v def '" -e "/\/\//i\'\; \\ \\ \n"|head -n-4|tr -d "\n" > $HOME/logs/mmww${time}.log; cp $HOME/logs/mmww${time}.log $HOME/logs/mmww.log; 
+w3m -dump  https://www.merriam-webster.com/word-of-the-day|head -n66|grep ":" -m1 -A22|tr -s "\n\t" "\n." |sed -e /play/d -e /Prev\ Next/d -e /What\ It\ Means/d -e "/Context.*/d"|sed -e "s/\: / printf -v da\ '\n/g" -e 1a"';\n\n printf -v w '" -e "2a';\n\n printf -v meaning '" -e 4c"'; \n\n printf -v def '" -e "/\/\//i\'\; \\ \\ \n"|grep -v '//'|head -n-4|tr -d "\n" > $HOME/logs/mmww${time}.log; cp $HOME/logs/mmww${time}.log $HOME/logs/mmww.log; 
