e02a4937ee93cbd7973da5031cd73e0c8fbd78bf
u0_a64 <u0_a64@localhost>
2025-04-10 23:55:18 +0200

    2025-04-10_23_55

---
+++ tmp/ss/batl.sh
@@ -0,0 +1,174 @@
+ARM Assembly
+Comma Separated Values
+Git Log
+JavaScript (Rails)
+Nix
+Ruby Haml
+TypeScript
+ASP
+Command Help
+Git Mailmap
+Java Properties
+OCaml
+Ruby Slim
+TypeScriptReact
+AWK
+CpuInfo
+Git Rebase Todo
+Java Server Page (JSP)
+OCamllex
+Ruby on Rails
+Verilog
+ActionScript
+Crontab
+Go
+Jinja2
+OCamlyacc
+Rust
+VimHelp
+Ada
+Crystal
+GraphQL
+Julia
+Objective-C
+SCSS
+VimL
+Apache Conf
+D
+Graphviz (DOT)
+Known Hosts
+Objective-C++
+SML
+Vue Component
+AppleScript
+Dart
+Groff/troff
+Kotlin
+PHP
+SQL
+Vyper
+AsciiDoc (Asciidoctor)
+Diff
+Groovy
+LLVM
+Pascal
+SQL (Rails)
+WGSL
+Assembly (x86_64)
+Dockerfile
+HTML
+LaTeX
+Perl
+SSHD Config
+XML
+Authorized Keys
+DotENV
+HTML (ASP)
+Lean
+Plain Text
+SSH Config
+YAML
+Batch File
+Elixir
+HTML (EEx)
+Less
+PowerShell
+Salt State (SLS)
+Zig
+BibTeX
+Elm
+HTML (Erlang)
+Lisp
+Protocol Buffer
+Sass
+fstab
+Bourne Again Shell (bash)
+Email
+HTML (Jinja2)
+Literate Haskell
+Protocol Buffer (TEXT)
+Scala
+gnuplot
+C
+Erlang
+HTML (Rails)
+LiveScript
+Puppet
+Solidity
+group
+C#
+F#
+HTML (Tcl)
+Lua
+PureScript
+Strace
+hosts
+C++
+Fish
+HTML (Twig)
+MATLAB
+Python
+Stylus
+jsonnet
+CFML
+Fortran Namelist
+HTTP Request and Response
+Makefile
+QML
+Svelte
+log
+CMake
+Fortran (Fixed Form)
+Haskell
+Manpage
+R
+Swift
+nginx
+CMakeCache
+Fortran (Modern)
+Highlight non-printables
+Markdown
+Racket
+SystemVerilog
+orgmode
+CMake C++ Header
+GLSL
+INI
+MediaWiki
+Rd (R Documentation)
+TOML
+passwd
+CMake C Header
+Git Attributes
+JQ
+MemInfo
+Rego
+Tcl
+reStructuredText
+CSS
+Git Commit
+JSON
+NAnt Build File
+Regular Expression
+TeX
+resolv
+Cabal
+Git Config
+Java
+NSIS
+Requirements.txt
+Terraform
+syslog
+Clojure
+Git Ignore
+JavaScript
+Nim
+Robot Framework
+Textile
+varlink
+CoffeeScript
+Git Link
+JavaScript (Babel)
+Ninja
+Ruby
+Todo.txt
