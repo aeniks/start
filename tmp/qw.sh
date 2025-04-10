a5965c1f0120240fc5166e3f9d97594c7ea07240
u0_a64 <u0_a64@localhost>
2025-04-10 03:51:40 +0200

    2025-04-10_03_51

---
+++ tmp/qw.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+## display 
+qwqw() { 
+local IFS=$'\n\t '; ff=($(cat $HOME/figz.sh|shuf)); h=55; fn=1; 
+printf %b "\e[$((LINES - 2));$((COLUMNS / 4))H\e7"; 
+while read -n1 -s k; do case $k in 
+ A) [[ $fn < 99 ]]&& n="\e[38;5;$((h++))m"; printf %b "\e8\e[K$n 	${ff[$h]} " ;; 
+ C) [[ $fn < 99 ]]&& n="\e[38;5;$((h++))m"; printf %b "\e8\e[K$n 	${ff[$h]} " ;; 
+ B) [[ $fn > 1 ]] || n="\e[0;2m";((h--));   printf %b "\e8\e[K$n 	${ff[$h]} " ;; 
+ D) [[ $fn > 1 ]] || n="\e[0;2m";((h--));   printf %b "\e8\e[K$n 	${ff[$h]} " ;; 
+"") printf %b "\e8\e[A\e[1J\e[2H"; figlet -f "${ff[$h]}" -w $COLUMNS -c "${ff[$h]}"|bat -ppflzig; ((h++)) ;; 
+ q) printf %b "\e8\e[K$n ${ff[$h]} \n"; break;; 
+esac; done; 
+}; 
