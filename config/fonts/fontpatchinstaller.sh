4d28d9634a26044594ba65eed08a9ce7cfad8cab
u0_a64 <u0_a64@localhost>
2025-04-13 00:57:17 +0200

    2025-04-13_00_57

--- config/fonts/fontpatchinstaller.sh
+++ config/fonts/fontpatchinstaller.sh
@@ -2,5 +2,5 @@
 ## font patcher
 sudo apt install -y node python3 python3-fontforge fontforge argparse 2>/dev/null; 
 npm i devicons -g;
-fontforge --script ./fontpatcher/font-patcher  ./fontpatcher/HackNerdFontMono-Regular.ttf 
-fontforge --script ./fontpatcher/font-patcher  /usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf
+fontforge --script $start/config/fonts/fontpatcher/font-patcher  ./fontpatcher/HackNerdFontMono-Regular.ttf 
+fontforge --script $start/config/fonts/fontpatcher/font-patcher  /usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf
