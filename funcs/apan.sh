4b97b62757fa995604d1d58054cd3dd67cce85c9
u0_a64 <u0_a64@localhost>
2025-04-19 09:04:29 +0200

    2025-04-19_09_04

--- funcs/apan.sh
+++ funcs/apan.sh
@@ -3,8 +3,8 @@ local IFS=$'\n '; unset sudo; [ -z $PREFIX ] && sudo=sudo;
 [ -d $HOME/logs/apts ] && [[ $(wc -l $HOME/logs/apa.log 2>/dev/null) < 22 ]] && \
 ($sudo apt update 2>/dev/null && $sudo apt list 2>/dev/null|cut -f1 -d"/" > $HOME/logs/apa.log); 
 apap=($(cat $HOME/logs/apa.log|\
-fzf -m -i --bind q:abort --inline-info --wrap --cycle --preview \
-"$sudo apt show {} 2>/dev/null|bat -ppflzig"))
+fzf -m -i --bind q:abort --inline-info --cycle --preview \
+"$sudo apt show {} 2>/dev/null|bat -ppflzig" --preview-window "wrap"))
 [ -z $apap ] && printf %b "\n $c2 OK\n\n" && return 0; 
 ######
 ######
