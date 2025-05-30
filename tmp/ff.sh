880b51ad7e56870288c8be5da2b72a90f34c8338
u0_a26 <u0_a26@localhost>
2025-05-30 22:09:12 +0200

    2025-05-30_22_09

---
+++ tmp/ff.sh
@@ -0,0 +1,2 @@
+
+command ls|fzf -m -i --preview "bat -ppflzig -f {}" --inline-info --ansi --expect enter --cycle --scroll-off 222 --bind a:select-all,q:abort,space:select+down --highlight-line --height ~88% 
--- tmp/wl/dic.sh
+++ tmp/wl/dic.sh
@@ -1,10 +1,10 @@
 
-wohtml() { 
+dics() { 
+dic="$@"; [ -z $dic ]&& printf %b "\n\n\n\n\e[4A -- enter word:"; read -ep ' ' dic; 
 mkdir -p $HOME/logs/wotd 2>/dev/null; 
-time="$(date +%y_%m_%d)"; 
-wotmp=$HOME/tmp/wotd_$time.log; 
-[ -z $1 ]&& ext="word-of-the-day"||ext="thesaurus/$1"; 
-command w3m -cols 800 -dump https://www.merriam-webster.com/$ext > $wotmp; 
+time="$(date +%H_%M_%S)"; 
+wotmp=$HOME/tmp/wotd_$dic.log; 
+command w3m -cols 800 -dump https://www.merriam-webster.com/thesaurus/$dic > $wotmp; 
 ( printf %b 'printf -v "w_w" "'; 
 cat $wotmp|sed -n 39p|cut -f1 -d" "|tr -d "\n"; 
 printf %b '";'; echo; ) > $HOME/logs/wotd_m.sh; 
@@ -24,12 +24,11 @@ font-family:monospace;font-size:
 clamp(2vmin,2.4vmin,.182em);
 text-align:center;padding-top:
 clamp(6vh,22vh,8em);margin-top:calc(12vh - 9ch);background:rgb($((RANDOM%88 + 8)),$((RANDOM%88 + 8)),$((RANDOM%88 + 8)));" > $wo.html; 
+
+
 sed -i '6i<meta name="viewport" content="width=device-width, initial-scale=1.0" />' $wo.html; 
-cd $HOME/gh/aeniks.github.io/wotd; 
-cp $HOME/logs/wotd/$wo.html $HOME/gh/aeniks.github.io/wotd/; 
-cp -f $wo.html index.html; 
-git add --all; git commit --all -m $(date +%F_%H_%M); git push; 
-sleep 4; rm $wo.png; 
+
+# cd $HOME/gh/aeniks.github.io/wotd; cp $HOME/logs/wotd/$wo.html $HOME/gh/aeniks.github.io/wotd/; cp -f $wo.html index.html; git add --all; git commit --all -m $(date +%F_%H_%M); git push; sleep 4; rm $wo.png; 
 ARGS=(
 --data-urlencode "key=408f3d" \
 --data-urlencode "dimension=1600x1600" \
@@ -46,4 +45,4 @@ cp -f $wo.png wotd.png;
 git add --all; git commit --all -m $(date +%F_%H_%M); git push; 
 }; 
 wotd_m() { time=$(date +%y_%m_%d); [ -e $HOME/logs/wotd/wotd_m_${time}.sh ] || . $start/crons/wotd_m.sh; . $HOME/logs/wotd_m.sh; printf %b "$yellow$w_w$pink > $re[$cyan$w_p${re}]$pink > $blue$w_t$pink >$re$dim \n\b\b$w_m"|bat -ppljava; };
-wohtml 
+ 
