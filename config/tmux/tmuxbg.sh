0019e40656eb08802f40baae7289dfa0166a2518
u0_a26 <u0_a26@localhost>
2025-06-07 04:41:14 +0200

    2025-06-07_04_41

--- config/tmux/tmuxbg.sh
+++ config/tmux/tmuxbg.sh
@@ -1,4 +1,4 @@
 tmuxbg() { 
-tmux source-file /data/data/com.termux/files/home/.tmux.conf; 
+tmux source-file ~/.tmux.conf; 
 tmux set-option -g status-style bg=colour$(printf %b "$((RANDOM%222))";); 
 } 
