e0965982fc5c3ffda89319ff7150833bebd9fe7e
u0_a26 <u0_a26@localhost>
2025-06-23 00:38:44 +0200

    2025-06-23_00_38

--- funcs/cleanse.sh
+++ funcs/cleanse.sh
@@ -6,7 +6,7 @@ dim='\e[2m' q="$(printf %b "\e[0;2m--\e[0m";)";
 unset filelist new newchange gg; 
 ## perl function 
 #newf() { echo -n $1 | perl -pe 's/[\?\[\]\/\\=<>:;,''"@&\$#*()|~`!{}%+]//g;' -pe 's/[\r\n\t -]+/_/g;'; }; 
-newf() { echo -n $1 | tr -d '%&/""?@!*[]{}()<>~/$|,:;`=*#\’'; }; 
+newf() { echo -n $1 | tr -d '%&/"" ?@!*[]{}()<>~/$|,:;`=*#\’'; }; 
 
 # perl -pe 's/[\?\[\]\/\\=<>:;,''"@&\$#*()|~`!{}%+]//g;' 
 # -pe #'s/[\r\n\t -]+/_/g;'; }; 
