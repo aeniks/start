be6aeb9d132c117e4c117c6f33b6ea9e30ce3eae
u0_a26 <u0_a26@localhost>
2025-06-27 23:49:41 +0200

    2025-06-27_23_49

--- funcs/apa.sh
+++ funcs/apa.sh
@@ -9,25 +9,29 @@ hash sudo 2>/dev/null && sudo=sudo;
 # pwd="${PWD}"; 
 printf %b "\n.... to update run [apass] ....\n"|\
 bat -ppflc --theme Dracula; 
-cd $HOME/logs/apts &>/dev/null; 
-aapp=($(ls|fzf --query " $1" -i -m --color \
-preview-bg:0 --bind "q:abort" --preview "cat {}" \
---preview-window "wrap,8,noborder,top" \
+#cd 
+# &>/dev/null; 
+aapp=($(ls $HOME/logs/apts|fzf --query " $1" -i -m --color \
+preview-bg:0 --bind "q:abort" --preview "cat $HOME/logs/apts/{}" \
+--preview-window "wrap,6,noborder,top" \
 --cycle --ansi --inline-info))||\
 (printf %b "\n\n"&& cd $pwd &>/dev/null&& return 1;)||\
-return 1; cd - &>/dev/null && \ 
+return 1; cd - &>/dev/null && \
 printf %b "\n\n"; (printf %b " -- Install: "; 
-printf %b "${aapp[*]}"|tr -s "\n" ","; bb="\b"; 
+printf %b "${aapp[*]}"|tr -s "\n" ","; bb=" \b"; 
 [[ ${#aapp[*]} == 1 ]]&& unset bb; 
 printf %b "$bb ? [Y/n] ")|bat -ppflc --theme Nord; 
-cd - &>/dev/null; 
-read -s -n1 "ny"; if [ -z $ny ]; then 
+cd $OLDPWD &>/dev/null; 
+read -s -n1 "ny"; if [ -z $ny ]; then cd $OLDPWD; 
 printf %b "\e[92m OK\e[0m \n\n"; 
-cd - &>/dev/null; 
-$sudo apt update; $sudo apt upgrade -y; 
-$sudo apt install -y ${aapp[*]}; 
-$sudo apt autoremove -y; 
-else cd - &>/dev/null; 
-printf %b "\n\$aapp\n"; return 0; fi; 
-printf %b "${aapp[*]}\n\n";
+$sudo apt update 2>/dev/null|bat -ppflc --theme Dracula; 
+$sudo apt upgrade -y 2>/dev/null|bat -ppflc --theme Dracula; 
+$sudo apt install -y ${aapp[*]} 2>/dev/null|bat -ppflc --theme Dracula; 
+$sudo apt autoremove -y 2>/dev/null|bat -ppflc --theme Dracula; 
+fi; 
+cd $OLDPWD &>/dev/null; printf %b "\n\n\${aapp[*]}\n\n"; 
+printf %b "${aapp[*]}"|tr -s "\n" " ";
+echo; echo; 
+return 0; 
+cd $OLDPWD; 
 }; 
--- funcs/ff.sh
+++ funcs/ff.sh
@@ -1,26 +1,42 @@
 # ripgrep->fzf->vim [QUERY]
 # unalias ff; 
-ff() ( 
+ff() { 
+local IFS=$'\n'; 
 hash rg 2>/dev/null||$sudo apt install ripgrep; 
 hash bat 2>/dev/null||$sudo apt install bat; 
-  RELOAD='reload:rg --column --color=always --smart-case {q} || :'
-  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
-            micro {1} +{2}    
-             # No selection. Open the current line in Vim.
-          else
-            micro {+f}  
-            # Build quickfix list for the selected items.
-          fi'
-  fzf -i --cycle --disabled --ansi --multi \
+RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then micro {1} +{2}; 
+else micro {+f}; fi'; 
+old="$PWD"; 
+dd=($(cd $HOME/logs/appa 2>/dev/null; fzf -i --cycle --disabled --ansi --multi \
       --bind "change:$RELOAD" \
+      --bind "start:$RELOAD" \
       --bind "ctrl-o:execute:$OPENER" \
       --bind "q:abort" \
       --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
       --delimiter : \
       --preview 'bat -p --color=always --highlight-line {2} {1}' \
-      --preview-window '<44(9),<80(up,5,wrap)' \
-      # --query "$*"
-)
+      --preview-window '<70(9),<80(up,2,wrap)' \
+)); 
+cd $old; 
+echo; 
+pp=($(for i in ${dd[*]}; do 
+printf %b "${i/:*/}\n"; 
+done; )); 
+printf %b "\e[96m --\e[0m Install: \n\n${pp[*]}\n"; 
+printf %b "\n\n\n\n\n\e[4A \e[0m\e[96m--\e[0m Continue? ";
+printf %b "\e[0\[\e[2mY\e[0m/\e[2mn\e[0m]";
+read -n1 -rsp ' ' 'yno';
+if [[ "$yno" == "${yno#[Nn]}" ]]; then \
+        printf %b "\e[92m OK\e[0m \n\n";
+        $sudo apt update 2> /dev/null | bat -ppflc --theme Dracula;
+        $sudo apt upgrade -y 2> /dev/null | bat -ppflc --theme Dracula;
+        $sudo apt install -y ${pp[*]} 2> /dev/null | bat -ppflc --theme Dracula;
+        $sudo apt autoremove -y 2> /dev/null | bat -ppflc --theme Dracula;
+else ( printf %b "nope\n\n";
+return 1 ); fi; 
+echo;       # --query "$*"
+}
       # --bind "enter:become:$OPENER" \
 
       # --bind "start:$RELOAD" 
