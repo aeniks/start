577e4e0317057a391786794578902922b09d06d6
u0_a79 <u0_a79@localhost>
2025-07-07 06:53:05 +0200

    2025-07-07_06_53

--- anew.sh
+++ anew.sh
@@ -237,5 +237,5 @@ else ipp=' \e[0;2m[\e[0;38m_\e[0;2m]\e[0m '; fi;
 . $start/_ps1.sh; _ps1; 
 bc=0; 
 # pastel list |sed -n $((RANDOM%139))p|pastel color 
-crond 2>/dev/null; 
-sshd 2>/dev/null; 
+# crond 2>/dev/null; 
+# sshd 2>/dev/null; 
