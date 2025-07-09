d3b3a31170b8d02db78fda115169cfd6e6b9babf
aa <aa@aeniks>
2025-07-10 00:38:27 +0200

    aa

--- funcs/12sshfix.sh
+++ funcs/12sshfix.sh
@@ -1,2 +1,3 @@
-
-chown $USER: $HOME/.ssh -R; chmod 700 $HOME/.ssh; chmod 600 $HOME/.ssh/*; chmod 644 $HOME/.ssh/*.pub; ls -pal --color=always $HOME/.ssh  
+12ssh_fix() { 
+chown $USER: $HOME/.ssh -R; chmod 700 $HOME/.ssh; chmod 600 $HOME/.ssh/*; chmod 644 $HOME/.ssh/*.pub; ls -pal --color=always $HOME/.ssh;   
+}; 
