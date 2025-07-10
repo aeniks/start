1600e41f9569ffe17f93c2f4414b02601372cf99
u0_a79 <u0_a79@localhost>
2025-07-10 11:02:31 +0200

    2025-07-10_11_02

---
+++ config/tmux/plugins/tpm/tpm_ins.sh
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+git clone https://github.com/tmux-plugins/tpm ~/tmux/plugins/tpm; 
+
+
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+run '~/tmux/plugins/tpm/tpm'
+
+
+
+
+tmux source ~/.tmux.conf
