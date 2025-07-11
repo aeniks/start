0014b2a92486c8858f2065c5d35f7b66f3807add
u0_a26 <u0_a26@localhost>
2025-07-11 15:24:34 +0200

    2025-07-11_15_24

---
+++ sh/uurl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+uuoo() { local IFS=$'\n\t '; unset ll; declare -a ll; ll=($(for i in $(tmux list-panes | cut -c1-2 | rev|tr ":" " "); do tmux capture-pane -Jqpt$i|tr "; " "\n" |grep -oE "(http?s://)?([\da-z\.-]+)\.([a-z\.]{0,12})([/\w?=.-].*)"|sed -e "s/\ /\n/g"|tr -s " " " "; done|sort -u;)); uuoo=$(printf %b "${ll[*]}"|fzf --tmux "center,98%,$(($(printf %b "${ll[*]}"|wc -l)+4))" --sync; ); [ $uuoo ] || return 0; xdg-open $uuoo 2>/dev/null || termux-open-url $uuoo 2>/dev/null || termux-open $uuoo 2>/dev/null || (printf %b "\n\n\n\n\e[A4A\n"; read -p' -- open with: ' -r "op"; printf %b "  ok\n"; $op $uuoo; ); };
+uuoo
