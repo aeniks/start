add37a5b0d2f51523a153ca18f9990821ad32946
aa <aa@aeniks.com>
2025-07-18 18:41:27 +0200

    2025-07-18_18_41

--- funcs/aimg.sh
+++ funcs/aimg.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 aimg() { 
-uq=${EPOCHSECONDS:5}; 
 printf %b "\n\n\n\n\e[4A\n"; prompt=(${@}); 
 [ -z $1 ] && printf %b "  -- gen pic " && read -e "prompt"; 
+####
+uq="${EPOCHSECONDS:5}"; out="${prompt/\ /}${uq}"; 
 ####
 curl https://api.cloudflare.com/client/v4/accounts/$CLOUDFLARE_ACCOUNT_ID/ai/run/@cf/lykon/dreamshaper-8-lcm \
 -X POST \
 -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
--d '{ "prompt": "${prompt[*]}" }' >> ${prompt/\ /}${uq}.jpg; 
+-d '{ "prompt": "${prompt[*]}" }' >> $out.jpg
 ####
 printf %b "\n\n\n\n\e[4A\n${prompt/\ /}${uq}.jpg\n"; 
+realpath ${out}.jpg; 
+printf %b "\n\n\n\n"; 
 ####
 }; 
