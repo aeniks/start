12fe944cae7f3a016e0ab3b4181504e5b6e0d0e8
u0_a79 <u0_a79@localhost>
2025-07-18 16:22:42 +0200

    2025-07-18_16_22

--- sh/uurl.sh
+++ sh/uurl.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-uuoo() { local IFS=$'\n\t '; unset ll; declare -a ll; ll=($(for i in $(tmux list-panes | cut -c1-2 | rev|tr ":" " "); do tmux capture-pane -Jqpt$i|tr "; " "\n" |grep -oE "(http?s://)?([\da-z\.-]+)\.([a-z\.]{0,12})([/\w?=.-].*)"|sed -e "s/\ /\n/g"|tr -s " " " "; done|sort -u;)); uuoo=$(printf %b "${ll[*]}"|fzf --tmux "center,98%,$(($(printf %b "${ll[*]}"|wc -l)+4))" --sync; ); [ $uuoo ] || return 0; printff %b "uuoo"|grep -E "http|https" || hh='https://'; 
-termux-open-url $hh$uuoo 2>/dev/null || \
+uuoo() { local IFS=$'\n\t '; unset ll; declare -a ll; ll=($(for i in $(tmux list-panes | cut -c1 | rev; ); do tmux capture-pane -Jqpt$i|\
+grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*"|sed -e "s/\ /\n/g"|tr -s " " " "; done; sort -u;));
+
+# tr "; " "\n" |grep -oE "(http?[s]://)?([\da-z\.-]+)\.([a-z\.]{0,12})([/\w?=.-].*)"|sed -e "s/\ /\n/g"|tr -s " " " "; done|sort -u;)); 
+
+uuoo=$(printf %b "${ll[*]}"|fzf --tmux "center,98%,$(($(printf %b "${ll[*]}"|wc -l)+4))" --sync; ); 
+[ $uuoo ] || return 0; 
+# printff %b "uuoo"|grep -E 'http://|https://' || hh='https://'; 
+termux-open-url $uuoo 2>/dev/null || \
 termux-open $uuoo 2>/dev/null || \
 xdg-open $uuoo 2>/dev/null || \
-open $hh$uuoo 2>/dev/null || \
+open $uuoo 2>/dev/null || \
 (printf %b "\n\n\n\n\e[A4A\n"; read -p' -- open with: ' -r "op"; printf %b "  ok\n"; $op $uuoo; ); };
 uuoo
